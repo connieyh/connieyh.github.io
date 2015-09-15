a20ddccc159afc2a814e9a774641334aa52f218d
YiHsuan Chen <Connie@YiHsuans-MacBook-Pro.local>
2015-09-15 10:46:30 -0400

    v1_flipped

--- index.js
+++ index.js
@@ -10,10 +10,43 @@
     clickListener( card );
   }
 
+
   function clickListener(card) {
     card.addEventListener( "click", function() {
       var c = this.classList;
-      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      if(c.contains("flipped")) {
+        c.add("flipped");
+      }
+      function clickOrigin(e){
+        var target = e.target;
+        var tag = [];
+        tag.tagType = target.tagName.toLowerCase();
+        tag.tagClass = target.className.split(' ');
+        tag.id = target.id;
+        tag.parent = target.parentNode;
+    
+        return tag;
+      }
+
+      var tagsToIdentify = ['a'];
+      var flag = false;
+
+      document.body.onclick = function(e){
+        elem = clickOrigin(e);
+        for (i=0;i<tagsToIdentify.length;i++){
+          if (elem.tagType == tagsToIdentify[i]){
+            flag = true;
+            console.log("a clicked?");
+          }
+        }
+        if(!flag){
+          c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");  
+        }
+      };
+      
+
+      
+      
     });
   }
 })();
